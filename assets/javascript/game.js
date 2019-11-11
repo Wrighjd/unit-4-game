cfc4d2ff09b5aa1848ba5d23fc6fd7ea5c44735f
Jarom Wright <jaromwright@gmail.com>
2019-11-10 22:41:53 -0700

    Info for crystal and new game functions.

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+
+const CrystalCollector = function() {
+    const numPages      = $(".page").length;
+    let   currentPage   = 0;
+    
+    const numCrystals   = 4;
+    let   crystalValues = new Array(numCrystals);
+    
+    
+    let numWins = 0, numLosses = 0;
+    let targetSum, currentSum;
+}
+    
+    this.startNewGame = function() {
+       
+        targetSum  = 0;
+        currentSum = 0;
+
+        
+        for (let i = 0; i < numCrystals; i++) {
+            crystalValues[i] = randomInteger(1, 12);
+
+           
+            targetSum += randomInteger(1, 6) * crystalValues[i];
+        }
